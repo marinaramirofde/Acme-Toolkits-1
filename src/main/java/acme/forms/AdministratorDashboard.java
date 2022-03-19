852da8b3000da769b151a7b158e0c94666ad6204
juaalvcam <juaalvcam@alum.us.es>
2022-03-19 01:36:57 +0100

    Task-037-R1: update

--- src/main/java/acme/forms/AdministratorDashboard.java
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -1,6 +1,9 @@
 package acme.forms;
 
 import java.io.Serializable;
+import java.util.Map;
+
+import org.springframework.data.util.Pair;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,47 +16,35 @@ public class AdministratorDashboard implements Serializable {
 
 	// Attributes -------------------------------------------------------------
 
-	Integer totalNumberOfComponents; //total number of components
+	int totalNumberOfComponents;
+	Map<Pair<String,String>,Double> averageRetailPriceOfComponentsByTechnologyAndCurrency;
+	Map<Pair<String,String>,Double> deviationRetailPriceOfComponentsByTechnologyAndCurrency;
+	Map<Pair<String,String>,Double> minimumRetailPriceOfComponentsByTechnologyAndCurrency;
+	Map<Pair<String,String>,Double>  maximumRetailPriceOfComponentsByTechnologyAndCurrency;
 	
-	Double averageRetailPriceOfComponents; //average retail price of components grouped by technology and currency
-	Double deviationRetailPriceOfComponents; //deviation retail price of components grouped by technology and currency
-	Double minimumRetailPriceOfComponents; //minimum retail price of components grouped by technology and currency
-	Double maximumRetailPriceOfComponents; //maximum retail price of components grouped by technology and currency
-	
-	Integer totalNumberOfTools; //total number of tools
-	
-	Double averageRetailPriceOfTools; //average retail price of Tools grouped by currency
-	Double deviationRetailPriceOfTools; //deviation retail price of Tools grouped by currency
-	Double minimumRetailPriceOfTools; //minimum retail price of Tools grouped by currency
-	Double maximumRetailPriceOfTools; //maximum retail price of Tools grouped by currency
-	
-	Integer totalNumberOfProposedPatronagres; // total number of proposed patronages
-	Integer totalNumberOfAcceptedPatronagres; // total number of accepted patronages
-	Integer totalNumberOfDeniedPatronagres; // total number of denied patronages
-	
-	//average, deviation, minimum, and maximum budget of proposed patronages.
-	
-	Double averageBudgetOfProposedPatronages;
-	Double deviationBudgetOfProposedPatronages;
-	Double minimumBudgetOfProposedPatronages;
-	Double maximumBudgetOfProposedPatronages;
-	
-	//average, deviation, minimum, and maximum budget of accepted patronages
-	
-	Double averageBudgetOfAcceptedPatronages;
-	Double deviationBudgetOfAcceptedPatronages;
-	Double minimumBudgetOfAcceptedPatronages;
-	Double maximumBudgetOfAcceptedPatronages;
-	
-	//average, deviation, minimum, and maximum budget of denied patronages
-	
-	Double averageBudgetOfDeniedPatronages;
-	Double deviationBudgetOfDeniedPatronages;
-	Double minimumBudgetOfDeniedPatronages;
-	Double maximumBudgetOfDeniedPatronages;
+	int totalNumberOfTools;
+	Map<String,Double> averageRetailPriceOfToolsByCurrency;
+	Map<String,Double> deviationRetailPriceOfToolsByCurrency;
+	Map<String,Double> minimumRetailPriceOfToolsByCurrency;
+	Map<String,Double> maximumRetailPriceOfToolsByCurrency;
 
-	// Derived attributes -----------------------------------------------------
+	int totalNumberOfProposedPatronagres;
+	int totalNumberOfAcceptedPatronagres;
+	int totalNumberOfDeniedPatronagres;
 
-	// Relationships ----------------------------------------------------------
+	double averageBudgetOfProposedPatronages;
+	double deviationBudgetOfProposedPatronages;
+	double minimumBudgetOfProposedPatronages;
+	double maximumBudgetOfProposedPatronages;
+
+	double averageBudgetOfAcceptedPatronages;
+	double deviationBudgetOfAcceptedPatronages;
+	double minimumBudgetOfAcceptedPatronages;
+	double maximumBudgetOfAcceptedPatronages;
+
+	double averageBudgetOfDeniedPatronages;
+	double deviationBudgetOfDeniedPatronages;
+	double minimumBudgetOfDeniedPatronages;
+	double maximumBudgetOfDeniedPatronages;
 
 }
