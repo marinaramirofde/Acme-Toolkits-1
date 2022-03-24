e4dddd7917398c1fa5871ac5e235289362550539
rubsuadav <rsuarezdavid@gmail.com>
2022-03-24 22:19:46 +0100

    Task 002: update

--- src/main/java/acme/features/administrator/item/AdministratorItemRepository.java
+++ src/main/java/acme/features/administrator/item/AdministratorItemRepository.java
@@ -14,7 +14,7 @@ public interface AdministratorItemRepository extends AbstractRepository {
 	@Query("select i from Item i where i.id = :id")
 	Item findOneComponentById(int id); //SHOW BY ID (COMPONENT)
 
-	@Query("select i from Item i")
+	@Query("select i from Item i where i.typeEntity =  acme.entities.items.Type.COMPONENT")
 	Collection<Item> findAllComponents();
 
 }
