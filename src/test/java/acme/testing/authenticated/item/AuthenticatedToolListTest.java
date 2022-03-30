b6833ea30a913090e2f4efb1cf8fc573d88467f7
rubsuadav <rsuarezdavid@gmail.com>
2022-03-30 14:14:46 +0200

    Task 003/T1: done

---
+++ src/test/java/acme/testing/authenticated/item/AuthenticatedToolListTest.java
@@ -0,0 +1,43 @@
+package acme.testing.authenticated.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AuthenticatedToolListTest extends TestHarness{
+
+	// Lifecycle management ---------------------------------------------------
+
+	// Test cases -------------------------------------------------------------
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/item/list-tools.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String typeEntity, final String name, final String code, final String technology, final String description, final String retailPrice, final String link) {
+		super.signIn("administrator", "administrator");
+
+		super.clickOnMenu("Authenticated", "List all tools");
+		super.checkListingExists();
+		
+		super.checkColumnHasValue(recordIndex, 0, typeEntity);
+		super.checkColumnHasValue(recordIndex, 1, name);
+		super.checkColumnHasValue(recordIndex, 2, code);
+		super.checkColumnHasValue(recordIndex, 3, technology);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("typeEntity", typeEntity);
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", technology);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("link", link);
+
+		super.signOut();
+	}
+
+
+}
