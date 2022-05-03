d0c3d1b6902e52f0bb5213a3e2d0f61dbf009e8c
MRomalde <migromdor@alum.us.es>
2022-05-03 21:26:45 +0200

    Update tests and extend the coverage

--- src/test/java/acme/testing/patron/patronages/PatronPatronageListMineTest.java
+++ src/test/java/acme/testing/patron/patronages/PatronPatronageListMineTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class PatronPatronageListMineServiceTest extends TestHarness {
+public class PatronPatronageListMineTest extends TestHarness {
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/patron/patronage/list-mine.csv", encoding = "utf-8", numLinesToSkip = 1)
