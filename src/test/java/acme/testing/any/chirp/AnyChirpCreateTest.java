e6a6d5124aa2242a497dca7ce95aabf0c88a9f12
nicsanmen <nicsanmen@alum.us.es>
2022-05-19 11:49:08 +0200

    Task 014: added negative test cases on chirps

--- src/test/java/acme/testing/any/chirp/AnyChirpCreateTest.java
+++ src/test/java/acme/testing/any/chirp/AnyChirpCreateTest.java
@@ -11,7 +11,9 @@ public class AnyChirpCreateTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirp/create-positive.csv", encoding = "utf-8",numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String title, final String author, final String body, final String email, final String confirmation) {
+	public void positiveTest(final int recordIndex, final String title, final String author, final String body, 
+		final String email, final String confirmation) {
+		
 		super.clickOnMenu("Anonymous","List recent chirps");
 		super.checkListingExists();
 		super.clickOnButton("Create");
@@ -21,6 +23,26 @@ public class AnyChirpCreateTest extends TestHarness{
 		super.fillInputBoxIn("email", email);
 		super.fillInputBoxIn("confirmation", confirmation);
 		super.clickOnSubmit("Create");
+		
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/chirp/create-negative.csv", encoding = "utf-8",numLinesToSkip = 1)
+	@Order(10)
+	public void negativeTest(final int recordIndex, final String title, final String author, final String body, 
+		final String email, final String confirmation) {
+		
+		super.clickOnMenu("Anonymous","List recent chirps");
+		super.checkListingExists();
+		super.clickOnButton("Create");
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("email", email);
+		super.fillInputBoxIn("confirmation", confirmation);
+		super.clickOnSubmit("Create");
+		super.checkErrorsExist();
+		
 	}
 	
 }
